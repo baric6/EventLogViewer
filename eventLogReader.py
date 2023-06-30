45e7b42ce8086d2aba22489593869465989cba8e
baric <31894871+baric6@users.noreply.github.com>
2023-06-30 13:33:26 -0400

    Add files via upload

---
+++ eventLogReader.py
@@ -0,0 +1,39 @@
+import pandas as pd
+import json
+
+class EventStorage:
+    def __init__(self, df):
+        self.df = df
+
+    def getDF(self):
+        print(self.df)
+
+    def getHead(self):
+        print(self.df.head())
+
+    def exportToJson(self):
+        json_list = df.to_dict(orient='records')
+         # Extract the log_string from the DataFrame
+        log_string = self.df['Task Category'].iloc[0]
+        
+        # Convert the log string to a dictionary
+        log_dict = {"Task Category": log_string.split("\r\n\r\n")}
+        
+        # Add the log dictionary to the JSON list
+        json_list.append(log_dict)
+        
+        with open('export.json', 'w') as f:
+            f.write(json.dumps(json_list, indent=2))
+
+def readEventLogs():    
+    # Read CSV files from List
+    df = pd.concat(map(pd.read_csv, ['eventLogs/TLSecurityEvents.csv']))
+    return df
+
+if __name__ == "__main__":
+    df = readEventLogs()
+    eventStorage = EventStorage(df)
+    #eventStorage.getDF()
+    #eventStorage.getHead()
+    #eventStorage.getColumnByName("Event ID")
+    eventStorage.exportToJson()
